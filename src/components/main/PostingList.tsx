edc1e63f2b624162f76e2a8b1e75c2f2ae6f31ca
pparkssang93 <pparkssang1013@gmail.com>
2024-09-13 18:11:14 +0900

    ✳️feat_ 예외처리 추가
    - 데이터에 아무 값도 없을 경우, 리스트 화면에 대한 예외처리 추가

--- src/components/main/PostingList.tsx
+++ src/components/main/PostingList.tsx
@@ -7,6 +7,14 @@ import MainCategory from "./MainCategory";
 function PostingList() {
     const root = postingListData[0];
 
+    if (!root.hasChild) {
+        return (
+            <div className="border rounded-md grow-[1] p-6 bg-white min-h-[819px] max-h-[819px] min-w-[273px] max-w-[273px] overflow-y-scroll hide-scrollbar">
+                <h1 className="font-black">nothing.</h1>
+            </div>
+        );
+    }
+
     return (
         <div
             id="posting_list-wrapper"
