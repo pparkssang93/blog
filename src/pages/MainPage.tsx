fa7455fd542aa80816a0bbb901ccf4319411e90c
pparkssang93 <pparkssang1013@gmail.com>
2024-09-13 18:12:39 +0900

    ✳️feat_ Main page

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/layout/Header";
+import Posting from "../components/main/Posting";
+
+function MainPage() {
+    return (
+        <>
+            <Header />
+            <Posting />
+        </>
+    );
+}
+
+export default MainPage;
