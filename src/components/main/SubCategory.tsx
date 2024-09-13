e8ae87e503e8182df778d4ce95bf8c394fd837c0
pparkssang93 <pparkssang1013@gmail.com>
2024-09-13 15:46:10 +0900

    ✳️feat_ 포스팅 이동 링크 추가

--- src/components/main/SubCategory.tsx
+++ src/components/main/SubCategory.tsx
@@ -1,14 +1,19 @@
+import { Link } from "react-router-dom";
 // TYPE
 import { IPostingList } from "../../data/postingData";
 
 export default function SubCategory({ child, data }: { child: number[]; data: IPostingList }) {
     return (
         <ul className="ml-8">
-            {child.map((ele) => (
-                <li key={ele} className="leading-10">
-                    {data[ele].postingTitle}
-                </li>
-            ))}
+            {child.map((ele) => {
+                if (typeof data[ele].link === "string") {
+                    return (
+                        <li key={ele} className="leading-8 text-sm">
+                            <Link to={data[ele].link}>{data[ele].postingTitle}</Link>
+                        </li>
+                    );
+                }
+            })}
         </ul>
     );
 }
