771c36e9498652a5310c3c149282af7b496398df
pparkssang93 <pparkssang1013@gmail.com>
2024-09-13 17:33:45 +0900

    ✳️feat_ Root 경로 컴포넌트
    - 임시로 설정해 놓은 컴포넌트
    - 게시물이 추가되면 최근 게시물 등 다양한 기능 추가 예정

--- src/components/main/Posting.tsx
+++ src/components/main/Posting.tsx
@@ -2,6 +2,18 @@
 import { Outlet, useLocation } from "react-router-dom";
 import PostingList from "./PostingList";
 
+const RootPage = function () {
+    return (
+        <div className="grow-[6] bg-white p-16 max-w-[880px] border rounded-md">
+            <i className="iFaceAdd text-8xl text-slate-500 iSemiBold"></i>
+            <h3 className="text-4xl font-black mt-10">
+                <i className="iArrowDropLeft text-4xl mr-2 "></i>
+                게시글을 선택해주세요.
+            </h3>
+        </div>
+    );
+};
+
 const Posting = function () {
     const location = useLocation();
     const path = location.pathname;
@@ -10,7 +22,7 @@ const Posting = function () {
         <main className="bg-slate-50 p-10">
             <div className="flex max-w-screen-xl mx-auto gap-10">
                 <PostingList />
-                {path === "/" ? <div className="grow-[6]">path</div> : <Outlet />}
+                {path === "/" ? <RootPage /> : <Outlet />}
             </div>
         </main>
     );
